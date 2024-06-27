74870db5d452f94cc120cc169e5f21beefb19a74
Aruroturo <arturo.alfaro3@my.vcccd.edu>
2024-06-26 22:21:38 -0700

    done

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def main():
     number1 = random.randint(0, 100)
     number2 = random.randint(0, 100)
     number3 = random.randint(0, 100)
+    min_value = number1
+    if number2 < min_value:
+        min_value = number2
+        if number3 < min_value:
+            min_value = number3
+    print(f'{number1} {number2} {number3}' )
+    print(min_value)
     """
     ########################################
     Code Your Program here
